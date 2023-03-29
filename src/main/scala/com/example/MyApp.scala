0b46992f79e7a4d83ed9dc9a66dfa96dacdfc748
Marijan Bebek <marijan.bebek@datamir.info>
2023-03-29 08:35:53 +0200

    Added singleton object for creating and/ or getting Spark session.

--- src/main/scala/com/example/MyApp.scala
+++ src/main/scala/com/example/MyApp.scala
@@ -1,9 +1,6 @@
 package com.example
-
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.SparkConf
 import org.apache.log4j.{Level, Logger}
-import org.apache.spark.sql.functions._
+import com.example.SparkSessionBuilder
 import data_process.DataProcessor
 
 
@@ -12,25 +9,17 @@ object MyApp {
 
     Logger.getLogger("org").setLevel(Level.ERROR) //reduce compile output
     Logger.getLogger("akka").setLevel(Level.ERROR)
-    val conf = new SparkConf()
-      .setAppName("MyApp")
-      .setMaster("local[*]")
-
-    val spark = SparkSession
-      .builder()
-      .config(conf)
-      .config("dfs.client.read.shortcircuit.skip.checksum", "true")
-      .getOrCreate()
-
 
     val fileSeparator = java.io.File.separator
     val dataSources = Array(f"data$fileSeparator%sinput$fileSeparator%sgoogleplaystore_user_reviews.csv",
       f"data$fileSeparator%sinput$fileSeparator%sgoogleplaystore.csv")
 
-    val dataProcessor = new DataProcessor(spark, dataSources)
-    dataProcessor.show(dataProcessor.df_1)
-    dataProcessor.show(dataProcessor.df_2)
+    val spark = SparkSessionBuilder.getSparkSession
 
+    val dataProcessor = new DataProcessor(spark, dataSources)
+    // part 1
+    dataProcessor.show(dataProcessor.df_1)
+    //part 2
     dataProcessor.save(dataProcessor.df_2, "best_apps.csv")
 
   }
---
+++ src/main/scala/com/example/SparkSessionBuilder.scala
@@ -0,0 +1,20 @@
+package com.example
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.SparkConf
+object SparkSessionBuilder {
+
+  private val conf = new SparkConf()
+    .setAppName("MyApp")
+    .setMaster("local[*]")
+
+  private val spark = SparkSession
+    .builder()
+    .config(conf)
+    .config("dfs.client.read.shortcircuit.skip.checksum", "true")
+    .getOrCreate()
+
+  def getSparkSession: SparkSession = {
+    spark
+  }
+
+}
